21aef76fd2fb150c8f41e298f111fb96d525e136
Ashimkayal4 <ashimkayal4@gmail.com>
2024-10-22 00:31:25 +0600

    added hash and button into blog.jsx and styling some components

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,11 +1,11 @@
 import PropTypes from 'prop-types';
 const Blog = ({ blog }) => {
-   const { author_img ,title,cover,author,posted_date,reading_time } = blog;
+   const { author_img ,title,cover,author,posted_date,reading_time ,hashtags} = blog;
     return (
         <div className="border-2 p-2 m-2 rounded">
-            <img src={cover} alt={`cover picture of the title ${title}`} />
-            <div className="md:flex md:justify-between items-center">
-                <div className="flex items-center">
+            <img className='rounded-sm' src={cover} alt={`cover picture of the title ${title}`} />
+            <div className="md:flex md:justify-between items-center mt-4 mb-4">
+                <div className="flex items-center gap-2">
                     <img className="w-14" src={author_img} alt="" />
                     <div>
                         <h2>{author}</h2>
@@ -17,6 +17,12 @@ const Blog = ({ blog }) => {
                 </div>
             </div>
             <h2 className="text-2xl">{title}</h2>
+
+            <p className='mt-2'> {
+                hashtags.map((hash,idx) => <span key={idx} className='px-1'><a href=""> # {hash }</a></span>)
+            }</p>
+
+            <button className='border p-1 text-violet-700'>Mark as read</button>
         </div>
     );
 };
